546995474228469d3834a327d5ac9e35608d87d4
Valliammai-Se <valliammai.se@rootquotient.com>
2025-08-26 13:14:36 +0530

    send sms

--- index.ts
+++ index.ts
@@ -48,10 +48,10 @@ const DeliveredQuestions = [
 const firstQuestion = "You order has been placed. Would you like to schedule the delivery? Reply 1 for YES and 2 for NO OR 3 for CANCEL";
 const secondQuestion = "Your delivery is scheduled for today and will be delivered in you selected slot time? If yes, reply 1 else 2";
 const thirdQuestion = "Your product has been delivered. Reply 1 to CONFIRM and 2 to REPORT ISSUE";
-export async function sendSMS(customerId: number, questionNumber?: number) {
+export async function sendSMS(customerId: number, questionNumber?: number, msg?: string) {
   const customer = await getCustomerById(customerId);
   const question = questionNumber === 1 ? firstQuestion : questionNumber === 2 ? secondQuestion : questionNumber === 3 ? thirdQuestion : firstQuestion;
-  const message = `Hello ${customer.name}, ${question}`;
+  const message = msg ? msg : `Hello ${customer.name}, ${question}`;
   const sms = await twilioClient.messages.create({
     body: message,
     from: config.TWILIO_PHONE_NUMBER,
